1774386464dbc70e6540ea73999606888b4a7030
jonaspereirar <jonaspereirar@gmail.com>
2021-02-15 00:04:38 +0000

    feat add: product and search product

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,5 +1,58 @@
-export default function Search() {
+import { useRouter } from "next/router";
+import Link from 'next/link';
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+import SearchForm from "@/components/SearchForm";
+import SEO from "@/components/SEO";
+import { client } from "@/lib/prismic";
+import Prismic from 'prismic-javascript';
+import { Document } from 'prismic-javascript/types/documents';
+import PrismicDOM from 'prismic-dom';
+
+
+type ServerSideProps = {
+  searchResults: Document[];
+}
+
+export default function Search({ searchResults }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  const router = useRouter();
+
+  const { q } = router.query;
+
   return (
-    <h1>Search</h1>
+    <div>
+      <SEO title={`Results for: ${q}`} />
+
+      <SearchForm />
+
+      <section>
+        <h1>Results for: {q}</h1>
+        <ul>
+          {searchResults.map(product => {
+            return (
+              <li>
+                <Link href={`/catalog/products/${product.uid}`}>
+                  <a>{PrismicDOM.RichText.asText(product.data.title)}</a>
+                </Link>
+              </li>
+            )
+          })}
+        </ul>
+      </section>
+    </div>
   )
+}
+
+export const getServerSideProps: GetServerSideProps<ServerSideProps> = async (context) => {
+  const { q } = context.query;
+
+  const searchResults = await client().query([
+    Prismic.Predicates.at('document.type', 'product'),
+    Prismic.Predicates.fulltext('my.product.title', String(q))
+  ])
+
+  return {
+    props: {
+      searchResults: searchResults.results,
+    }
+  };
 }
