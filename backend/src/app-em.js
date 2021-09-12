24928213b545103b333190665c291e9d3db78d0d
jonaspereirar <jonaspereirar@gmail.com>
2021-09-12 22:25:52 +0100

    feat: miniTickerStream - testNet
    Signed-off-by: jonaspereirar <jonaspereirar@gmail.com>

---
+++ backend/src/app-em.js
@@ -0,0 +1,11 @@
+const crypto = require('./utils/crypto')
+
+module.exports = (settings) => {
+  settings.secretKey = crypto.decrypt(settings.secretKey);
+  const exchange = require('./utils/exchange')(settings);
+
+  exchange.miniTickerStream((markets) => {
+    console.log(markets);
+  })
+
+}
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,20 @@
 const database = require('./db');
 const app = require('./app');
+const settingsRepository = require('./repositories/settingsRepository');
+const appEm = require('./app-em')
 
-app.listen(process.env.PORT, () => {
-  console.log('App is running.')
+/* A api da Binence exige um settings default, para isso ele sobe por default
+o Settings de um usuario, Pasta Utils/exchange.js - arquivo app-em.js e server configurado para mini Ticker Stream
+https://binance-docs.github.io/apidocs/spot/en/#websocket-market-streams 
+*/
+settingsRepository.getDefaultSettings()
+.then(settings => {
+  appEm(settings);
+  app.listen(process.env.PORT, () => {
+    console.log('App is running.')
+  })
 })
+.catch(err => {
+  console.error(err)
+})
+
